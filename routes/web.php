d000d0c853a40a46fbb8c3f4692c5183727f32dd
unknown <lrzambrano@uniguajira.edu.co>
2021-08-06 09:18:40 -0500

    six commit

--- routes/web.php
+++ routes/web.php
@@ -21,12 +21,45 @@ $router->group(['prefix' => 'v1'], function () use ($router) {
 
    // Auth
    $router->post('usuarios', ['as' => 'users.store', 'uses' => 'UsuarioController@store']);
-   $router->post('signin/email', ['as' => 'signinWithEmail', 'uses' => 'UsuarioController@signInWithEmail']);
-   $router->post('signin/username', ['as' => 'signinWithUsername', 'uses' => 'UsuarioController@signInWithUsername']);
+   $router->post('login/email', ['as' => 'signinWithEmail', 'uses' => 'UsuarioController@signInWithEmail']);
+   $router->post('login/username', ['as' => 'signinWithUsername', 'uses' => 'UsuarioController@signInWithUsername']);
    $router->post('logout', ['as' => 'logout', 'uses' => 'UsuarioController@logout']);
 
    //Posts
-   $router->get('posts', ['as' => 'publicaciones', 'uses' => 'PublicacionController@index']);
+   $router->get('publicaciones', ['as' => 'publicaciones', 'uses' => 'PublicacionController@index']);
+
+   //Solicitud
+   $router->get('solicitudes', ['as' => 'solicitudes', 'uses' => 'SolicitudController@index']);
+   $router->post('solicitudes', ['as' => 'solicitudes.store', 'uses' => 'SolicitudController@store']);
+   $router->get('solicitudes/{id}', ['as' => 'solcitudes.show', 'uses' => 'SolitudController@show']);
+   $router->put('solicitudes/{id}', ['as' => 'solicitudes.update', 'uses' => 'SolicitudController@update']);
+   $router->delete('solicitudes/{id}', ['as' => 'solicitudes.delete', 'uses' => 'SolicitudController@delete']);
+
+   //Beneficiario
+   $router->get('beneficiarios', ['as' => 'beneficiarios', 'uses' => 'BeneficiarioController@index']);
+   $router->post('beneficiarios', ['as' => 'beneficiarios.store', 'uses' => 'BeneficiarioController@store']);
+   $router->get('beneficiarios/{id}', ['as' => 'beneficiarios.show', 'uses' => 'BeneficiarioController@show']);
+   $router->put('beneficiarios/{id}', ['as' => 'beneficiarios.update', 'uses' => 'BeneficiarioController@update']);
+   $router->delete('beneficiarios/{id}', ['as' => 'beneficiarios.delete', 'uses' => 'BeneficiarioController@delete']);
+
+   //Reacion
+   $router->get('reaciones', ['as' => 'reaciones', 'uses' => 'ReacionController@index']);
+   $router->get('reaciones/{id}', ['as' => 'reaciones.show', 'uses' => 'ReacionController@show']);
+   $router->post('reaciones', ['as' => 'reaciones.store', 'uses' => 'ReacionController@store']);
+   $router->put('reaciones/{id}', ['as' => 'reaciones.update', 'uses' => 'ReacionController@update']);
+   $router->delete('reaciones/{id}', ['as' => 'reaciones.delete', 'uses' => 'ReacionController@delete']);
+
+   //Triage
+   $router->get('triages', ['as' => 'triages', 'uses' => 'TriageController@index']);
+   $router->get('triages/{id}', ['as' => 'triages.show', 'uses' => 'TriageController@show']);
+   $router->post('triages', ['as' => 'triages.store', 'uses' => 'TriageController@store']);
+   $router->put('triages/{id}', ['as' => 'triages.update', 'uses' => 'TriageController@update']);
+   $router->delete('triages/{id}', ['as' => 'triages.delete', 'uses' => 'TriageController@delete']);
+
+   
+
+   
+
 
 });
 
